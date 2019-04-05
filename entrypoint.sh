d116d484a516440bf02b36d67454ec1a349a5acb
奚玉帆 <568875138@qq.com>
2019-04-05 10:08:43 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@ git clone https://github.com/switchbrew/libnx
 cd libnx
 make
 make install
-wget https://ave.zone/sept-secondary.enc -O /tmp/sept-secondary.enc
+cd ..
 make all
 make dist
+wget https://ave.zone/sept-secondary.enc -O /tmp/sept-secondary.enc
 cp /tmp/sept-secondary.enc out/
