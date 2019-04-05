897ea5ce8fcdab616d32ab7e67eebe9b637b3f8c
奚玉帆 <568875138@qq.com>
2019-04-05 10:07:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ make
 make install
 wget https://ave.zone/sept-secondary.enc -O /tmp/sept-secondary.enc
 make all
-cp /tmp/sept-secondary.enc sept/sept-secondary/sept-secondary.enc
 make dist
+cp /tmp/sept-secondary.enc out/
